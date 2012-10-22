a58c0b2862724d076db5a8a1792113341e97199c
Alex Kocharin <alex@kocharin.ru>
2012-10-22 20:27:18 +0400

    link to compiled js files for performance reasons

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 
-var cs_installed = false;
-try {
-	require('coffee-script');
-	cs_installed = true;
-} catch(err) {
-	module.exports = require('./lib/index');
-}
-
-if (cs_installed) module.exports = require('./src/index');
+module.exports = require('./lib/index');
 
